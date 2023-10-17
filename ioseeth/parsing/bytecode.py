50a470e1a7d33be0cba48603acf074478dc8ab10
apehex <apehex@protonmail.com>
2023-10-17 10:50:17 +0200

    Split the creation data into creation + runtime + meta + args

--- ioseeth/parsing/bytecode.py
+++ ioseeth/parsing/bytecode.py
@@ -37,6 +37,12 @@ def is_raw_hex(bytecode: str) -> bool:
     except Exception:
         return False
 
+# NORMALIZE ###################################################################
+
+def normalize(bytecode: str) -> str:
+    """Format the hex bytecode in a known and consistent way."""
+    return bytecode.lower().replace('0x', '')
+
 # REGEX #######################################################################
 
 def selector_regex(raw: bool=False) -> str:
@@ -57,23 +63,48 @@ def storage_slot_regex(raw: bool=False) -> str:
         r'7f([0-9a-f]{64})' if raw
         else r'PUSH32\s+([0-9a-f]{64})')
 
+def creation_regex() -> str:
+    """Regex matching the end of the creation bytecode, when it returns the runtime bytecode."""
+    return r'(f3(?:00|fe)?)(6080)'
+
 def metadata_regex() -> str:
     """Regex matching the metadata appended to the bytecode."""
-    return r'(a264697066735822([0-9a-f]{68})64736f6c6343([0-9a-f]{6})0033)'
+    return r'(a264697066735822[0-9a-f]{68}64736f6c6343[0-9a-f]{6}0033)'
+
+# CREATION ####################################################################
+
+def split_creation(bytecode: str) -> list:
+    """Split the creation and runtime code from the creation data."""
+    __parts = re.split(pattern=creation_regex(), string=bytecode, flags=re.IGNORECASE)
+    return (''.join(__parts[:2]), ''.join(__parts[2:]))
 
 # METADATA ####################################################################
 
 def split_metadata(bytecode: str) -> list:
     """Split the metadata from the bytecode, returning both."""
-    return re.split(pattern=metadata_regex(), string=bytecode)
+    return re.split(pattern=metadata_regex(), string=bytecode, flags=re.IGNORECASE)
 
-# NORMALIZE ###################################################################
+# PARSE CREATION DATA #########################################################
 
-def normalize(bytecode: str) -> str:
-    """Format the hex bytecode in a known and consistent way.
-    Remove both the prefix "0x" and the metadata suffix."""
-    __split = split_metadata(bytecode=bytecode)
-    return __split[0].lower().replace('0x', '')
+#TODO recursive parsing in case several contracts are deployed
+# 0xb61e1747cb5b2b9ff4a5dd18e625c1b5547a655d4d5136505e7cabd5e5299e93
+
+def parse_creation_data(data: str) -> tuple:
+    """Split the creation data into 4 parts: creation bytecode + runtime bytecode + metadata + creation args."""
+    __creation = __runtime = __metadata = __args = ''
+    __creation, __rest = split_creation(bytecode=data)
+    __parts = split_metadata(bytecode=__rest)
+    if __parts:
+        __runtime = __parts[0]
+    if len(__parts) > 1:
+        __metadata = __parts[1]
+    if len(__parts) > 2:
+        __args = __parts[2]
+    return (
+        normalize(__creation),
+        normalize(__runtime),
+        normalize(__metadata),
+        normalize(__args))
 
 # INSTRUCTIONS ################################################################
 
@@ -112,7 +143,7 @@ def bytecode_has_specific_opcodes(bytecode: str, opcodes: tuple, check: callable
 
 def get_function_selectors(bytecode: str, raw: bool=False) -> tuple:
     """Get all the function selectors from the hub portion of the bytecode."""
-    _r = re.compile(selector_regex(raw=raw), re.IGNORECASE)
+    _r = re.compile(selector_regex(raw=raw), flags=re.IGNORECASE)
     return tuple(set(_r.findall(bytecode)))
 
 # STORAGE #####################################################################
@@ -121,5 +152,5 @@ def get_function_selectors(bytecode: str, raw: bool=False) -> tuple:
 
 def get_storage_slots(bytecode: str, raw: bool=False) -> str:
     """Get all the storage slots used in the bytecode."""
-    _r = re.compile(storage_slot_regex(raw=raw), re.IGNORECASE)
+    _r = re.compile(storage_slot_regex(raw=raw), flags=re.IGNORECASE)
     return tuple(set(_r.findall(bytecode)))
