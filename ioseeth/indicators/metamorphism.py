05a8d883c87facbf17aedd817b396c4d8f3033d4
apehex <apehex@protonmail.com>
2023-10-25 21:59:11 +0200

    Fix typo that made the test always succeed...

--- ioseeth/indicators/metamorphism.py
+++ ioseeth/indicators/metamorphism.py
@@ -5,9 +5,9 @@ import ioseeth.parsing.bytecode
 # KNWON #######################################################################
 
 INIT_CODES = (
-    '5860208158601c335a63aaf10f428752fa158151803b80938091923cf3')
+    '5860208158601c335a63aaf10f428752fa158151803b80938091923cf3',)
 
-# OPCODES #####################################################################
+# CREATION CODE ###############################################################
 
 def bytecode_has_known_metamorphic_init_code(bytecode: str) -> bool:
     """Check whether the creation bytecode contains known init code to setup metamorphic contracts."""
